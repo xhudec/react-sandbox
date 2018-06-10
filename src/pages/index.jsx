91ead801c7a2a54e49e8ccaa16ceef0cbcdc279e
xhudec <l.hudec75@gmail.com>
2018-06-10 13:07:01 +0200

    Basic project layout

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,1 +1,42 @@
 // @flow
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Header from '../components/Header';
+import SideBar from '../components/SideBar';
+import LandingPage from './LandingPage';
+
+import * as Styled from './styledComponents';
+
+type Props = {};
+
+type State = {
+  sidebarIsOpen: boolean,
+};
+
+class Root extends Component<Props, State> {
+  state = {
+    sidebarIsOpen: false,
+  };
+
+  openSideBar = () => this.setState({ sidebarIsOpen: true });
+  closeSideBar = () => this.setState({ sidebarIsOpen: false });
+
+  render() {
+    const { sidebarIsOpen } = this.state;
+
+    return (
+      <Styled.Layout>
+        <Header openSideBar={this.openSideBar} />
+        <SideBar isOpen={sidebarIsOpen} onClose={this.closeSideBar} />
+        <Styled.Body>
+          <Switch>
+            <Route path="/" component={LandingPage} />
+          </Switch>
+        </Styled.Body>
+      </Styled.Layout>
+    );
+  }
+}
+
+export default Root;
---
+++ src/pages/styledComponents.js
@@ -0,0 +1,12 @@
+// @flow
+import styled from 'styled-components';
+
+export const Layout = styled.div`
+  margin: 0;
+  padding: 0;
+`;
+
+export const Body = styled.div`
+  margin-top: 120px;
+  padding: 0 20%;
+`;
