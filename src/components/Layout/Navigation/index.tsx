9bd101ff01c2d76b560108f300beba285b290214
Lukáš Hudec <lukashudec@Lukass-MacBook-Pro.local>
2019-06-07 11:01:53 +0200

    refactor(navigation): Link component composition
    Created a new component which behaves as an adapter between
    `react-router-dom`'s `Link` component and `@material-ui` List
    components.
    see: https://material-ui.com/guides/composition/#react-router-demo

--- src/components/Layout/Navigation/index.tsx
+++ src/components/Layout/Navigation/index.tsx
@@ -1,58 +1,44 @@
 import React from 'react'
 import Divider from '@material-ui/core/Divider'
 import List from '@material-ui/core/List'
-import ListItem from '@material-ui/core/ListItem'
-import ListItemIcon from '@material-ui/core/ListItemIcon'
-import ListItemText from '@material-ui/core/ListItemText'
-import Slide from '@material-ui/core/Slide'
 import IconCode from '@material-ui/icons/Code'
 import IconSchedule from '@material-ui/icons/Schedule'
 import IconSettings from '@material-ui/icons/Settings'
 
-import Link from 'components/Link'
+import ListItemLink from './ListItemLink'
 
-import { TRANSITION_DELAY, TIMEOUT } from 'utils/constants'
+const navigationLinks = [
+  {
+    icon: <IconCode color="action" />,
+    label: 'Hooks',
+    href: '/hook-examples',
+  },
+  {
+    icon: <IconSchedule color="action" />,
+    label: 'Suspense',
+    href: '/suspense-examples',
+    disabled: true,
+  },
+  {
+    icon: <IconSettings color="action" />,
+    label: 'Experimental',
+    href: '/experimental',
+    disabled: true,
+  },
+]
 
 function NavigationList() {
   return (
     <List component="nav">
-      <Slide in direction="right" timeout={TIMEOUT}>
-        <ListItem button>
-          <ListItemIcon>
-            <IconCode color="action" />
-          </ListItemIcon>
-          <ListItemText primary="Hooks" />
-        </ListItem>
-      </Slide>
+      {navigationLinks.map(navigationLink => (
+        <ListItemLink
+          disabled={navigationLink.disabled}
+          icon={navigationLink.icon}
+          label={navigationLink.label}
+          to={navigationLink.href}
+        />
+      ))}
 
-      <Slide
-        in
-        direction="right"
-        timeout={TIMEOUT}
-        style={{ transitionDelay: `${TRANSITION_DELAY}ms` }}
-      >
-        <ListItem disabled button>
-          <ListItemIcon>
-            <IconSchedule color="action" />
-          </ListItemIcon>
-          <ListItemText primary="Suspense" />
-        </ListItem>
-      </Slide>
-      <Link to="/experimental">
-        <Slide
-          in
-          direction="right"
-          timeout={TIMEOUT}
-          style={{ transitionDelay: `${TRANSITION_DELAY * 2}ms` }}
-        >
-          <ListItem button>
-            <ListItemIcon>
-              <IconSettings color="action" />
-            </ListItemIcon>
-            <ListItemText primary="Experimental" />
-          </ListItem>
-        </Slide>
-      </Link>
       <Divider />
     </List>
   )
